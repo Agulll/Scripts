aa80c043a223a5e0284448ab57b686895579d525
Agulll <61034995+Agulll@users.noreply.github.com>
2022-09-18 19:45:10 +0200

    Update DL.sh
    typos

--- DL.sh
+++ DL.sh
@@ -6,43 +6,44 @@ cd ~/desktop
 mkdir $HASH
 cd $HASH
 pwd
-if [ -z "$SELECTION"];
-then
-    echo "No selection."
-    if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
-      then
-        echo "Downloading audio."
-        youtube-dl --yes-playlist --add-metadata --metadata-from-title "%(artist)s - %(title)s" -f "bestaudio[asr<=44100]" --embed-thumbnail -x --audio-format mp3 $LINK
-    fi
-    if [ $PARAMETERS = 'v' ] || [ $PARAMETERS = 'V' ];
-      then
-        echo "Downloading video."
-        youtube-dl --yes-playlist --add-metadata --embed-thumbnail f bestaudio+bestvideo --merge-output-format mp4 $LINK
-    fi
-    if [ $PARAMETERS = 't' ] || [ $PARAMETERS = 'T' ];
-      then
-        echo "Downloading the thumbnail."
-        youtube-dl --yes-playlist --write-thumbnail $LINK
-    fi
-else
-    if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
-      then
-        echo "Downloading audios."
-        youtube-dl --yes-playlist --add-metadata --metadata-from-title "%(artist)s - %(title)s" -f "bestaudio[asr<=44100]" --embed-thumbnail -x --audio-format mp3 --playlist-items $SLECTION $LINK
+if ["$SELECTION" = ""];
+  then
+      echo "No selection."
+      if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
+        then
+          echo "Downloading audio."
+          youtube-dl --yes-playlist --add-metadata --metadata-from-title "%(artist)s - %(title)s" -f "bestaudio[asr<=44100]" --embed-thumbnail -x --audio-format mp3 $LINK
+      fi
+      if [ $PARAMETERS = 'v' ] || [ $PARAMETERS = 'V' ];
+        then
+          echo "Downloading video."
+          youtube-dl --yes-playlist --add-metadata --embed-thumbnail f bestaudio+bestvideo --merge-output-format mp4 $LINK
+      fi
+      if [ $PARAMETERS = 't' ] || [ $PARAMETERS = 'T' ];
+        then
+          echo "Downloading the thumbnail."
+          youtube-dl --yes-playlist --write-thumbnail $LINK
+      fi
+  else
+      echo "Selection is ${SELECTION}"
+      if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
+        then
+          echo "Downloading audios."
+          youtube-dl --yes-playlist --playlist-items "$SELECTION" --add-metadata --metadata-from-title "%(artist)s - %(title)s" -f "bestaudio[asr<=44100]" --embed-thumbnail -x --audio-format mp3 $LINK
 
-    fi
-    if [ $PARAMETERS = 'v' ] || [ $PARAMETERS = 'V' ];
-      then
-        echo "Downloading videos."
-        youtube-dl --yes-playlist --add-metadata --embed-thumbnail f bestaudio+bestvideo --merge-output-format mp4 --playlist-items $SLECTION $LINK
-    fi
-    if [ $PARAMETERS = 't' ] || [ $PARAMETERS = 'T' ];
-      then
-        echo "Downloading the thumbnails."
-        youtube-dl --yes-playlist --write-thumbnail --playlist-items $SLECTION $LINK
-    fi
+      fi
+      if [ $PARAMETERS = 'v' ] || [ $PARAMETERS = 'V' ];
+        then
+          echo "Downloading videos."
+          youtube-dl --yes-playlist --playlist-items "$SELECTION" --add-metadata --embed-thumbnail f bestaudio+bestvideo --merge-output-format mp4 $LINK
+      fi
+      if [ $PARAMETERS = 't' ] || [ $PARAMETERS = 'T' ];
+        then
+          echo "Downloading the thumbnails."
+          youtube-dl --yes-playlist --playlist-items "$SELECTION" --write-thumbnail $LINK
+      fi
 fi
-if [$PARAMETERS = 'a' ] || [ $PARAMETERS = 'A'];
+if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
   then
     echo "Lauching piacrd"
     open -a "picard" *.mp3
@@ -53,4 +54,4 @@ if [$PARAMETERS = 'a' ] || [ $PARAMETERS = 'A'];
     cd ../
     rm -R $HASH
 fi
-ehco "Exiting"
+echo "Exiting"
