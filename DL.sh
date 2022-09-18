e67021bf7ee9f97a4df82c2aad9f19346ad5aef0
Agulll <61034995+Agulll@users.noreply.github.com>
2022-09-18 19:24:41 +0200

    Update DL.sh
    Better code

--- DL.sh
+++ DL.sh
@@ -1,33 +1,56 @@
 #!/bin/bash
-echo "Input settings [Link, a/v/t Selection]"
-read $LINK $PARAMETERS $SLECTION
-$HASH = md5($LINK)
+echo "Input Link ; parameters {a/v/t} ; selection: "
+read LINK PARAMETERS SELECTION
+HASH="$(echo "${LINK}" | md5sum | sed 's/  -$//')"
 cd ~/desktop
 mkdir $HASH
 cd $HASH
-if [-z $SLECTION]
-then if [$PARAMETERS = 'a' || $PARAMETERS = 'A']
-then youtube-dl --add-metadata --metadata-from-title "%(artist)s - %(title)s" --embed-thumbnail -x --audio-format mp3 -f bestaudio[asr<=44100] $LINK
-fi if [$PARAMETERS = 'v' || $PARAMETERS = 'V']
-then youtube-dl --add-metadata --embed-thumbnail f bestaudio+bestvideo --format mp4 $LINK
-fi if [$PARAMETERS = 't' || $PARAMETERS = 'T']
-then youtube-dl --write-thumbnail $LINK
-fi else if [$PARAMETERS = 'a' || $PARAMETERS = 'A']
-then youtube-dl --add-metadata --metadata-from-title "%(artist)s - %(title)s" --embed-thumbnail -x --audio-format mp3 -f bestaudio[asr<=44100] --playlist-items $SLECTION $LINK
-fi if [$PARAMETERS = 'v' || $PARAMETERS = 'V']
-then youtube-dl --add-metadata --embed-thumbnail f bestaudio+bestvideo --format mp4 --playlist-items $SLECTION $LINK
-fi if [$PARAMETERS = 't' || $PARAMETERS = 'T']
-then youtube-dl --write-thumbnail --playlist-items $SLECTION $LINK
-fi
-if [$PARAMETERS = 'a' || $PARAMETERS = 'A']
+pwd
+if [ -z "$SELECTION"];
 then
-echo "Lauching piacrd"
-open -a "picard" *.mp3
-read
-echo "Lauching itunes"
-open -a "itunes" *.mp3
-read
-cd ../
-rm -R $HASH
-ehco "Exiting"
+    echo "No selection."
+    if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
+      then
+        echo "Downloading audio."
+        youtube-dl --yes-playlist --add-metadata --metadata-from-title "%(artist)s - %(title)s" -f "bestaudio[asr<=44100]" --embed-thumbnail -x --audio-format mp3 $LINK
+    fi
+    if [ $PARAMETERS = 'v' ] || [ $PARAMETERS = 'V' ];
+      then
+        echo "Downloading video."
+        youtube-dl --yes-playlist --add-metadata --embed-thumbnail f bestaudio+bestvideo --merge-output-format mp4 $LINK
+    fi
+    if [ $PARAMETERS = 't' ] || [ $PARAMETERS = 'T' ];
+      then
+        echo "Downloading the thumbnail."
+        youtube-dl --yes-playlist --write-thumbnail $LINK
+    fi
+else
+    if [ $PARAMETERS = 'a' ] || [ $PARAMETERS = 'A' ];
+      then
+        echo "Downloading audios."
+        youtube-dl --yes-playlist --add-metadata --metadata-from-title "%(artist)s - %(title)s" -f "bestaudio[asr<=44100]" --embed-thumbnail -x --audio-format mp3 --playlist-items $SLECTION $LINK
+
+    fi
+    if [ $PARAMETERS = 'v' ] || [ $PARAMETERS = 'V' ];
+      then
+        echo "Downloading videos."
+        youtube-dl --yes-playlist --add-metadata --embed-thumbnail f bestaudio+bestvideo --merge-output-format mp4 --playlist-items $SLECTION $LINK
+    fi
+    if [ $PARAMETERS = 't' ] || [ $PARAMETERS = 'T' ];
+      then
+        echo "Downloading the thumbnails."
+        youtube-dl --yes-playlist --write-thumbnail --playlist-items $SLECTION $LINK
+    fi
 fi
+if [$PARAMETERS = 'a' ] || [ $PARAMETERS = 'A'];
+  then
+    echo "Lauching piacrd"
+    open -a "picard" *.mp3
+    read
+    echo "Lauching itunes"
+    open -a "itunes" *.mp3
+    read
+    cd ../
+    rm -R $HASH
+fi
+ehco "Exiting"
